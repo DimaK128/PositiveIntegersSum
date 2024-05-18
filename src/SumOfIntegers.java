766ae526745f5689a1b87bac572f8671f53540c8
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2024-05-18 20:01:52 +0300

    Initial commit

---
+++ src/SumOfIntegers.java
@@ -0,0 +1,17 @@
+public class SumOfIntegers {
+    public static void main(String[] args) {
+        int n = 5;
+        int result = sum1toN(n);
+        System.out.println("Sum of the first " + n + " positive integers is: " + result);
+    }
+
+    public static int sum1toN(int n) {
+        // Base case: if n equals 1, return 1
+        if (n == 1) {
+            return 1;
+        }
+
+        // Recursive case: sum(n) = n + sum(n - 1)
+        return n + sum1toN(n - 1);
+    }
+}
